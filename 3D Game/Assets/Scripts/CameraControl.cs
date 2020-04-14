a8c8c67e7d396466facedc3f7579a37f93a2bdb4
juies0608 <58469330+juies0608@users.noreply.github.com>
2020-04-14 21:36:34 +0800

    攝影機設定 程式

---
+++ 3D Game/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform dragon;
+
+    [Header("追蹤速度"), Range(0, 100)]
+    public float speed;
+    
+    /// <summary>
+    /// 追蹤飛龍
+    /// </summary>
+    private void Track()
+    {
+        Vector3 draPos = dragon.position;
+        draPos.y = 50;
+        draPos.z -= 9;
+       
+
+        transform.position = Vector3.Lerp(transform.position, draPos, 0.3f);
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
---
+++ 3D Game/Assets/Scripts/Dragon.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 300;
+    [Header("虛擬搖桿")]
+    public Joystick joy;
+    //第一種寫法:須要欄位
+    //public Transform tra;
+
+        /// <summary>
+        /// 移動
+        /// </summary>
+    public void Move()
+    {
+        print("移動中~");
+
+        //第一種寫法
+        //tra.Translate(0,0,1);
+        //Time.deltaTime 一幀的時間
+        //Input.GetAxis("Vertical"); - ws 上下
+        //水平:Horizontal
+
+        
+        float v = Input.GetAxis("Vertical");
+        float h= Input.GetAxis("Horizontal");
+
+        //第二種寫法:僅限於 Transform
+        transform.Translate(speed * Time.deltaTime * h, 0, speed * Time.deltaTime * v);
+
+
+        float joyV = joy.Vertical;
+        float joyH = joy.Horizontal;
+
+        transform.Translate(speed * Time.deltaTime * joyH, 0, speed * Time.deltaTime * joyV);
+
+        Vector3 pos = transform.position;       //取得飛龍座標
+        pos.x = Mathf.Clamp(pos.x, 30, 70);     //數學.夾住(值,最小,最大)
+        transform.position = pos;               //飛龍座標 = 夾住座標
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
