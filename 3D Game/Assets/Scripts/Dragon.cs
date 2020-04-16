ba74ca83e1ffbc608d20421979b45f5fa318cf0a
juies0608 <58469330+juies0608@users.noreply.github.com>
2020-04-16 21:35:08 +0800

    飛龍程式 火球設定

--- 3D Game/Assets/Scripts/Dragon.cs
+++ 3D Game/Assets/Scripts/Dragon.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Dragon : MonoBehaviour
 {
@@ -6,12 +7,29 @@ public class Dragon : MonoBehaviour
     public float speed = 300;
     [Header("虛擬搖桿")]
     public Joystick joy;
+    [Header("攻擊冷卻時間")]
+    public float cd = 1;
+    [Header("火球生成延遲時間")]
+    public float delayFire = 0.10f;
+    [Header("火球")]
+    public GameObject fierBall;
+    [Header("火球移動速度"), Range(1, 5000)]
+    public float speedFireBall = 300;
+
     //第一種寫法:須要欄位
     //public Transform tra;
 
-        /// <summary>
-        /// 移動
-        /// </summary>
+    /// <summary>
+    /// 動畫控制器
+    /// </summary>
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+    /// <summary>
+    /// 移動
+    /// </summary>
     public void Move()
     {
         print("移動中~");
@@ -37,11 +55,45 @@ public class Dragon : MonoBehaviour
 
         Vector3 pos = transform.position;       //取得飛龍座標
         pos.x = Mathf.Clamp(pos.x, 30, 70);     //數學.夾住(值,最小,最大)
+        pos.z = Mathf.Clamp(pos.z, 20, 30);
         transform.position = pos;               //飛龍座標 = 夾住座標
     }
 
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        timer += Time.deltaTime;            //計時器  遞增
+        if (timer >= cd)            //如果   計時器 >= 冷卻
+        {
+            timer = 0;                  //計時器  歸零
+            ani.SetTrigger("攻擊觸發");     //動畫控制.設定觸發器("參數名稱")
+
+            StartCoroutine(DelayFireBall()); 
+        }
+       
+    }
+    private  IEnumerator DelayFireBall()
+    {
+        yield return new WaitForSeconds(delayFire);
+
+        Vector3 posFire = transform.position;           //火球座標 = 飛龍座標
+            posFire.z = 25f;                               //微調x軸
+            posFire.y += 5.5f;                              //微調Y軸
+
+           GameObject temp = Instantiate(fierBall, posFire, Quaternion.identity);            //生成(物件、座標、角度)
+                                                                                             //Quaternion.identity Unity 角度類型、零角度
+        temp.GetComponent<Rigidbody>().AddForce(0, 0, speedFireBall);
+    }
+    private void Start()
+    {
+        //取得元件<泛型>()
+        ani = GetComponent<Animator>();
+    }
     private void Update()
     {
         Move();
+        Attack();
     }
 }
---
+++ 3D Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("地板陣列")]
+    //陣列:儲存多筆相同類型資料
+    public Transform[] terrains;
+    [Header("地板移動速度"), Range(1f, 50f)]
+    public float speedTerrain = 1.5f;
+
+    /// <summary>
+    /// 移動地板
+    /// </summary>
+    private void MoveTerrain()
+    {
+        //移動
+        // terrains[0].Translate(0, 0, -speedTerrain * Time.deltaTime);
+        //  terrains[1].Translate(0, 0, -speedTerrain * Time.deltaTime);
+
+        //如果地板.z 小於100
+        //  if (terrains[0].position.z <= -100)
+        //  {
+        //另一塊地板的前方 100位置
+        //    terrains[0].position = new Vector3(0, 0, terrains[1].position.z + 100);
+        // }
+
+        //  if (terrains[1].position.z <= -100)
+        // {
+        //      terrains[1].position = new Vector3(0, 0, terrains[0].position.z + 100);
+        //  }
+
+        // for (int i = 0; i < 10; i++)
+        // {
+        //    print("迴圈:" + i);
+        // }
+        for (int i = 0; i < terrains.Length; i++)
+        {
+            if (terrains[i].position.z <= -100)
+            {
+                terrains[i].position = new Vector3(0, 0, terrains[1 - i].position.z + 100);
+            }
+            terrains[i].Translate(0, 0, -speedTerrain * Time.deltaTime);
+        }
+
+    }
+    /// <summary>
+    /// 固定幀數更新事件:1秒50幀
+    /// </summary>
+    private void FixedUpdate()
+    {
+        MoveTerrain();
+    }
+}
